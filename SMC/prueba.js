eedb74adda95c2d01d6b1db737a18aaa4fb38971
Sasha <136343348+Sashits@users.noreply.github.com>
2023-08-07 10:10:10 -0300

    Add files via upload

---
+++ SMC/prueba.js
@@ -0,0 +1,22 @@
+console.log('La suma es: ' + (5 + 4))
+console.log('La resta es: ' + (5 - 4))
+console.log('La division es: ' + (5 / 4))
+console.log('La multiplicacion es: ' + (5 * 4))
+
+let variablesuma = 5+4
+console.log (variablesuma)
+alert (variablesuma)
+
+let variableresta = 5-4
+console.log (variableresta)
+alert (variableresta)
+
+let variabledivision = 5/4
+console.log (variabledivision)
+alert (variabledivision)
+
+let variablemultiplicaion = 5*4
+console.log (variablemultiplicaion)
+alert (variablemultiplicaion)
+
+alert (2023-2009)
